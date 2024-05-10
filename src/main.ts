0aed806a94479bc45ec37156e0ab42883bac2bb4
Camiloriwi <camilomanco2005@gmail.com>
2024-05-10 06:17:18 -0500

    update: main.ts to be able to use or submit data from the database through the reactjs form

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Continental Hotels API')
